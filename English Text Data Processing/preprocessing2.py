529c5857473eda90cc29640b3aacd05bf2485e5b
Kevin <gegaojian.richard@outlook.com>
2016-09-26 12:17:58 +0800

    readme

--- English Text Data Processing/preprocessing2.py
+++ English Text Data Processing/preprocessing2.py
@@ -13,11 +13,14 @@ import time
 
 
 def tokenization(fileName, stopWordList):
+    pattern = r'''(?x)\
+                ([A-Z]\.)+\
+                | \w+(-\w+)*\
+                | \$?\d+(\.\d+)?%?\
+                | \.\.\.
+                | [][.,;"'?():-_`]
+                '''
     wordList = []
-    # contentOfLines = open(fileName).readlines()
-    # for contentInLen in contentOfLines:
-    #     wordList.extend([stemming(token.lower()) for token in re.split(r'\W*', contentInLen) if len(token) > 2 and re.search(r'\w*\d+\w*', token) == None and token.lower() not in stopWordList])
-    # return wordList
     content = open(fileName).read()
     wordList.extend([stemming(token.lower()) \
                     for token in re.split(r'\W*', content) \
@@ -36,21 +39,21 @@ def stemming(word):
 # idfList: 每个元素对应词汇表中每个词在多少篇文章中出现过
 # vocabDict: 记录词汇表中每个词的顺序（位置）
 def computeTFIDF(wtotalList, tfList, idfDict, vocabDict):
-    # getcontext().prec = 50
     d = len(tfList)
     featSparseMatrix = []
     for i in range(d):
         print "Compute TFIDF : %d/%d" % (i+1, d)
         featSparseVect = [str(vocabDict[word]) + ":" + \
                         str(float(tfList[i][word]) / wtotalList[i] * log(d / idfDict[word])) \
-                        for word in sorted(tfList[i].keys()) if (float(tfList[i][word]) / wtotalList[i] * log(d / idfDict[word])) != 0.0]
+                        for word in sorted(tfList[i].keys()) \
+                        if (float(tfList[i][word]) / wtotalList[i] * log(d / idfDict[word])) != 0.0]
 
         featSparseMatrix.append(featSparseVect)
     return featSparseMatrix
 
 # 读取数据集，并完成分词、词干化，以及相关统计数据的采集
-def loadDataSet(fileName):
-    subFileList = listdir(fileName) # 子文件夹列表
+def loadDataSet(dataPath, stopWordPath):
+    subFileList = listdir(dataPath) # 子文件夹列表
     classList = []                  # 论文类别列表 classList[0] = 0 表示语料库第一篇论文的类别为0
     class2Int = []                  # 类别映射列表 class2Int[0] = "Active Learning" 表示用0表示类别Active Learning
     paperName2Int = []              # 论文映射列表 paperName2Int[0] = "Active Transfer Learning under Model Shift" 表示语料库中第一篇文章是Active Transfer Learning under Model Shift
@@ -60,13 +63,13 @@ def loadDataSet(fileName):
     idfDict = {}                    # 统计每个词在多少片文章中出现过
 
     # 读取停用词列表
-    stopWordList = open("stop-word-list.txt").readlines()
+    stopWordList = open(stopWordPath).readlines()
     stopWordList = [stopWord.strip("\r\n") for stopWord in stopWordList]
 
     classCount = len(subFileList) - 1 # 去掉.DS_Store文件
-    for i in range(1, classCount+1): # 第一个元素为隐藏的.DS_Store文件
+    for i in range(1, classCount+1):  # 第一个元素为隐藏的.DS_Store文件
         class2Int.append(subFileList[i].split(".")[1].strip())
-        fileList = listdir("/Users/gegaojian/Sources/Code/DataMiningWork/ICML/" + subFileList[i])
+        fileList = listdir(dataPath + subFileList[i])
         paperList = [paper for paper in fileList if paper.split('.')[1] == 'txt']
         paperName2Int.extend([paper.split('.')[0] for paper in fileList if paper.split('.')[1] == 'txt'])
 
@@ -74,7 +77,7 @@ def loadDataSet(fileName):
             tfDict = {}
             classList.append(i)
             print "Preprocessing paper : " + paperList[j].split('.')[0] # 打印论文名
-            wordList = tokenization("/Users/gegaojian/Sources/Code/DataMiningWork/ICML/" + subFileList[i] + "/" + paperList[j], stopWordList)
+            wordList = tokenization(dataPath + subFileList[i] + "/" + paperList[j], stopWordList)
             wtotalList.append(len(wordList))
             for word in wordList:
                 tfDict[word] = tfDict.get(word,0) + 1
@@ -89,7 +92,10 @@ def loadDataSet(fileName):
 
 def main():
 
-    paperName2Int, class2Int, classList, vocab2Int, wtotalList, tfList, idfDict = loadDataSet("/Users/gegaojian/Sources/Code/DataMiningWork/ICML")
+    dataPath = raw_input("请输入训练数据集的绝对路径(例：/Users/gegaojian/Sources/Code/DataMiningWork/ICML) ： ")
+    stopWordPath = raw_input("请输入停用词文件的路径(例：stop-word-list.txt) ： ")
+
+    paperName2Int, class2Int, classList, vocab2Int, wtotalList, tfList, idfDict = loadDataSet(dataPath, stopWordPath)
 
     # print len(paperName2Int)
     # print len(vocab2Int)
